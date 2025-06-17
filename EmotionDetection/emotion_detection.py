2b44385b342cadb02c860042f0148a0678d4da8a
madhavbihani32 <madhavbihani32@gmail.com>
2025-06-17 15:48:21 -0400

    all done

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,8 +7,20 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(URL, json=myobj, headers=header)
+    if response.status_code == 400:
+        emotion_dict= {}
+        emotion_dict['anger'] = None
+        emotion_dict['disgust'] = None
+        emotion_dict['fear'] = None
+        emotion_dict['joy'] = None
+        emotion_dict['sadness'] = None
+        emotion_dict['dominant_emotion'] = None
+
+        return emotion_dict
     formatted_text = json.loads(response.text)
     emotion_dict = formatted_text['emotionPredictions'][0]['emotion']
+    
+
     dominant_emotion = max(emotion_dict, key=emotion_dict.get)
     emotion_dict['dominant_emotion'] = dominant_emotion
     return emotion_dict
--- server.py
+++ server.py
@@ -1,4 +1,35 @@
-''''''
-from flask import Flask, render_template, request
-import requests
+"""Emotion Detector Flask App
+This module provides a web interface for emotion detection using Flask.
+"""
 
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    """Analyze the emotions in the provided text and return the results."""
+    text_to_analyze = request.args.get('textToAnalyze')
+    if text_to_analyze is None:
+        return {"error": "invalid/ or no data recieved"}
+    returned_dict = emotion_detector(text_to_analyze)
+    if returned_dict['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    return (
+        f"For the given statement, the system response is 'anger' : {returned_dict['anger']}, "
+        f"'disgust' : {returned_dict['disgust']}, "
+        f"'fear' : {returned_dict['fear']}, "
+        f"'joy' : {returned_dict['joy']}, "
+        f"'sadness' : {returned_dict['sadness']}. "
+        f"The dominant emotion is {returned_dict['dominant_emotion']}"
+    ), 200
+
+@app.route('/')
+def index_html_renderer():
+    """Render the index HTML page."""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+
+    app.run(host = 'localhost', port=5000)
